03aa0797f1d745c9009d48770f55272db49de651
Max Wang <max@covar.com>
2023-10-13 14:28:12 -0400

    Add necessary dependencies in package

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setup(
     name="ui-test-runner",
-    version="0.0.1",
+    version="0.0.2",
     author="Max Wang",
     author_email="max@covar.com",
     url="https://github.com/TranslatorSRI/UI_Test_Runner",
@@ -15,6 +15,10 @@ setup(
     long_description=readme,
     packages=["ui_test_runner"],
     include_package_data=True,
+    install_requires=[
+        "jq>=1.6.0",
+        "httpx>=0.25.0",
+    ],
     zip_safe=False,
     license="MIT",
     python_requires=">=3.9",
